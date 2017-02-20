3644cbefe39a8cbdc2c05368b767b200885fee3d
fangcm <fangcm2008@qq.com>
2017-02-20 23:02:47 +0800

    h

--- plugins/task/questions/Helper.cs
+++ plugins/task/questions/Helper.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text;
 using System.Xml.Serialization;
 
@@ -7,6 +8,16 @@ namespace Dragonfly.Questions
     public class Helper
     {
         public static void SaveExaminationToFile(string filePath, Examination exam)
+        {
+            SaveToFile(filePath, exam);
+        }
+
+        public static Examination LoadExaminationFromFile(string filePath)
+        {
+            return (Examination)LoadFromFile(filePath, typeof(Examination));
+        }
+
+        public static void SaveToFile(string filePath, object exam)
         {
             if (!string.IsNullOrWhiteSpace(filePath) && exam != null)
             {
@@ -18,16 +29,16 @@ namespace Dragonfly.Questions
             }
         }
 
-        public static Examination LoadExaminationFromFile(string filePath)
+        public static object LoadFromFile(string filePath, Type type)
         {
-            Examination result = null;
+            object result = null;
 
             if (File.Exists(filePath))
             {
                 using (StreamReader reader = new StreamReader(filePath, Encoding.UTF8))
                 {
-                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(Examination));
-                    result = (Examination)xmlSerializer.Deserialize(reader);
+                    XmlSerializer xmlSerializer = new XmlSerializer(type);
+                    result = xmlSerializer.Deserialize(reader);
                 }
             }
 
