7711d8ffd3adf45ff657e154ff0808f8f4997927
fangcm <fangcm2008>
2017-03-09 18:39:33 +0800

    setting

---
+++ plugins/task/plugin.task/AdjustmentConditionForm.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Dragonfly.Plugin.Task
+{
+    internal partial class AdjustmentConditionForm : Form
+    {
+        public bool bDataChanged = false;
+
+        public string Description
+        {
+            get { return this.textBoxDescription.Text; }
+            set { this.textBoxDescription.Text = value; }
+        }
+
+        public int IntervalMinutes
+        {
+            get { return Convert.ToInt32(this.numericUpDownInterval.Value); }
+            set { numericUpDownInterval.Value = value; }
+        }
+
+        public bool IsTooLateLockScreen
+        {
+            get { return this.checkBoxTooLate.Checked; }
+            set { checkBoxTooLate.Checked = value; }
+        }
+
+        public String TooLateTriggerTime
+        {
+            get { return this.dateTimePickerTooLate.Value.ToString("HH:mm"); }
+            set
+            {
+                DateTime settingTime;
+                try
+                {
+                    settingTime = DateTime.ParseExact(value, "HH:mm", null);
+                }
+                catch
+                {
+                    settingTime = DateTime.ParseExact("21:00", "HH:mm", null);
+                }
+                dateTimePickerTooLate.Value = settingTime;
+            }
+        }
+
+        public int TooLateMinutes
+        {
+            get { return Convert.ToInt32(this.numericUpDownTooLate.Value); }
+            set { numericUpDownTooLate.Value = value; }
+        }
+
+        public int NotifyInternalType
+        {
+            get
+            {
+                int ret;
+
+                if (radioButtonHibernate.Checked)
+                    ret = SettingHelper.NotifyInternalType_Hibernate;
+                else if (radioButtonShutdown.Checked)
+                    ret = SettingHelper.NotifyInternalType_ShutDown;
+                else
+                    ret = SettingHelper.NotifyInternalType_None;
+
+                return ret;
+            }
+            set
+            {
+                if (value == SettingHelper.NotifyInternalType_Hibernate)
+                    radioButtonHibernate.Checked = true;
+                else if (value == SettingHelper.NotifyInternalType_ShutDown)
+                    radioButtonShutdown.Checked = true;
+                else
+                    radioButtonNone.Checked = true;
+            }
+        }
+
+        public bool IsLockScreen
+        {
+            get { return this.checkBoxLockScreen.Checked; }
+            set { checkBoxLockScreen.Checked = value; }
+        }
+
+        public int LockScreenMinutes
+        {
+            get { return Convert.ToInt32(numericUpDownLockScreen.Value); }
+            set { numericUpDownLockScreen.Value = value; }
+        }
+
+        public int LockScreenApp
+        {
+            get { return this.checkBoxUseQuestionNotify.Checked ? 1 : 0; }
+            set { this.checkBoxUseQuestionNotify.Checked = (value == 1); }
+        }
+
+        public bool IsAppAdjustment
+        {
+            get { return this.checkBoxAdjustment.Checked; }
+            set { this.checkBoxAdjustment.Checked = value; }
+        }
+
+        public bool IsNotifyRunApp
+        {
+            get { return this.checkBoxRunApp.Checked; }
+            set { checkBoxRunApp.Checked = value; }
+        }
+
+        public string NotifyRunApp
+        {
+            get { return this.textBoxApp.Text; }
+            set { textBoxApp.Text = value; }
+        }
+
+        public string NotifyRunAppParam
+        {
+            get { return this.textBoxAppParam.Text; }
+            set { textBoxAppParam.Text = value; }
+        }
+
+        public string NotifyRunAppStartpath
+        {
+            get { return this.textBoxAppStartpath.Text; }
+            set { textBoxAppStartpath.Text = value; }
+        }
+
+        public JobSettingForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TaskSettingsForm_Load(object sender, EventArgs e)
+        {
+            listViewAdjustment.Columns.Clear();
+            listViewAdjustment.Columns.Add("说明", 110, HorizontalAlignment.Left);
+            listViewAdjustment.Columns.Add("调节方向", 70, HorizontalAlignment.Left);
+            listViewAdjustment.Columns.Add("条件来源", 80, HorizontalAlignment.Left);
+            listViewAdjustment.Columns.Add("条件值", 90, HorizontalAlignment.Left);
+            listViewAdjustment.Columns.Add("秒数", 40, HorizontalAlignment.Left);
+
+            SettingHelper helper = SettingHelper.GetInstance();
+            NotifyJobSetting setting = helper.PluginSetting.NotifyJobSetting;
+
+            Description = setting.Description;
+            IntervalMinutes = setting.IntervalMinutes;
+            IsTooLateLockScreen = setting.IsTooLateLockScreen;
+            TooLateTriggerTime = setting.TooLateTriggerTime;
+            TooLateMinutes = setting.TooLateMinutes;
+            IsLockScreen = setting.IsLockScreen;
+            LockScreenMinutes = setting.LockScreenMinutes;
+            LockScreenApp = setting.LockScreenApp;
+            NotifyInternalType = setting.NotifyInternalType;
+            IsAppAdjustment = setting.IsAppAdjustment;
+            IsNotifyRunApp = setting.IsNotifyRunApp;
+            NotifyRunApp = setting.NotifyRunApp;
+            NotifyRunAppParam = setting.NotifyRunAppParam;
+            NotifyRunAppStartpath = setting.NotifyRunAppStartpath;
+
+            if (helper.PluginSetting.AdjustmentSetting.Conditions != null)
+            {
+                foreach (AdjustmentCondition con in helper.PluginSetting.AdjustmentSetting.Conditions)
+                {
+                    AddAdjustmentCondition(con);
+                }
+            }
+
+            this.textBoxDescription.TextChanged += new System.EventHandler(this.Data_Changed);
+            this.numericUpDownInterval.ValueChanged += new System.EventHandler(this.Data_Changed);
+            this.checkBoxLockScreen.CheckedChanged += new System.EventHandler(this.Data_Changed);
+            this.numericUpDownLockScreen.ValueChanged += new System.EventHandler(this.Data_Changed);
+            this.checkBoxUseQuestionNotify.CheckedChanged += new System.EventHandler(this.Data_Changed);
+            this.radioButtonHibernate.CheckedChanged += new System.EventHandler(this.Data_Changed);
+            this.radioButtonShutdown.CheckedChanged += new System.EventHandler(this.Data_Changed);
+            this.radioButtonNone.CheckedChanged += new System.EventHandler(this.Data_Changed);
+            this.checkBoxAdjustment.CheckedChanged += new System.EventHandler(this.Data_Changed);
+            this.checkBoxRunApp.CheckedChanged += new System.EventHandler(this.Data_Changed);
+            this.textBoxApp.TextChanged += new System.EventHandler(this.Data_Changed);
+            this.textBoxAppParam.TextChanged += new System.EventHandler(this.Data_Changed);
+            this.textBoxAppStartpath.TextChanged += new System.EventHandler(this.Data_Changed);
+            this.checkBoxTooLate.CheckedChanged += new System.EventHandler(this.Data_Changed);
+            this.numericUpDownTooLate.ValueChanged += new System.EventHandler(this.Data_Changed);
+            this.dateTimePickerTooLate.ValueChanged += new System.EventHandler(this.Data_Changed);
+            bDataChanged = false;
+        }
+
+        private void Data_Changed(object sender, System.EventArgs e)
+        {
+            bDataChanged = true;
+            System.Diagnostics.Debug.WriteLine("Data_Changed:" + sender);
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            if (Description.Length == 0)
+            {
+                this.tabControl1.SelectedIndex = 0;
+                this.textBoxDescription.Focus();
+                MessageBox.Show(this, "请输入提醒的内容", "输入错误");
+                return;
+            }
+
+            if (bDataChanged)
+            {
+                SettingHelper helper = SettingHelper.GetInstance();
+                NotifyJobSetting setting = helper.PluginSetting.NotifyJobSetting;
+
+                setting.Description = Description;
+                setting.IntervalMinutes = IntervalMinutes;
+                setting.IsTooLateLockScreen = IsTooLateLockScreen;
+                setting.TooLateTriggerTime = TooLateTriggerTime;
+                setting.TooLateMinutes = TooLateMinutes;
+                setting.IsLockScreen = IsLockScreen;
+                setting.LockScreenMinutes = LockScreenMinutes;
+                setting.LockScreenApp = LockScreenApp;
+                setting.NotifyInternalType = NotifyInternalType;
+                setting.IsAppAdjustment = IsAppAdjustment;
+                setting.IsNotifyRunApp = IsNotifyRunApp;
+                setting.NotifyRunApp = NotifyRunApp;
+                setting.NotifyRunAppParam = NotifyRunAppParam;
+                setting.NotifyRunAppStartpath = NotifyRunAppStartpath;
+
+                List<AdjustmentCondition> conditions = helper.PluginSetting.AdjustmentSetting.Conditions;
+                if (conditions == null)
+                {
+                    conditions = new List<AdjustmentCondition>();
+                }
+                else
+                {
+                    conditions.Clear();
+                }
+
+                foreach (ListViewItem lvi in listViewAdjustment.Items)
+                {
+                    AdjustmentCondition con = lvi.Tag as AdjustmentCondition;
+                    if (con != null)
+                    {
+                        conditions.Add(con);
+                    }
+                }
+
+                helper.Save();
+            }
+
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void tsb_newAdjustment_Click(object sender, EventArgs e)
+        {
+            AdjustmentCondition con = new AdjustmentCondition()
+            {
+                Title = "wps ppt",
+                Accumulated = true,
+                ConditionType = 2,
+                ConditionValue = "wpp",
+                SpanSeconds = 50,
+            };
+
+            AddAdjustmentCondition(con);
+            Data_Changed(sender, e);
+        }
+
+        private void tsb_editAdjustment_Click(object sender, EventArgs e)
+        {
+            Data_Changed(sender, e);
+        }
+
+        private void tsb_deleteAdjustment_Click(object sender, EventArgs e)
+        {
+            if (listViewAdjustment.SelectedItems.Count == 0)
+            {
+                MessageBox.Show(this, "请选择要删除的调节项", "错误");
+                return;
+            }
+            if (MessageBox.Show(this, "确实要删除这些调节项吗？", "提示", MessageBoxButtons.YesNo) != DialogResult.Yes)
+            {
+                return;
+            }
+
+            foreach (ListViewItem lvi in listViewAdjustment.SelectedItems)
+            {
+                int index = lvi.Index;
+                listViewAdjustment.Items.RemoveAt(index);
+            }
+            Data_Changed(sender, e);
+        }
+
+        private void AddAdjustmentCondition(AdjustmentCondition con)
+        {
+            ListViewItem lvi = listViewAdjustment.Items.Add(con.Title);
+            lvi.SubItems.Add(con.Accumulated ? "持续递减" : "锁屏延时");
+
+            string type = string.Empty;
+            //0 filename, 1 title, 2 processname
+            switch (con.ConditionType)
+            {
+                case 0:
+                    type = "文件名";
+                    break;
+                case 1:
+                    type = "窗口标题";
+                    break;
+                case 2:
+                    type = "进程名称";
+                    break;
+
+            }
+            lvi.SubItems.Add(type);
+            lvi.SubItems.Add(con.ConditionValue);
+            lvi.SubItems.Add(con.SpanSeconds.ToString());
+            lvi.Tag = con;
+        }
+
+        private void labelInterval_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDownInterval_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ plugins/task/plugin.task/AdjustmentConditionForm.designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Dragonfly.Plugin.Task
+{
+    partial class AdjustmentConditionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.numericUpDownInterval = new System.Windows.Forms.NumericUpDown();
+            this.labelMinute = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButtonNone = new System.Windows.Forms.RadioButton();
+            this.radioButtonHibernate = new System.Windows.Forms.RadioButton();
+            this.radioButtonShutdown = new System.Windows.Forms.RadioButton();
+            this.textBoxApp = new System.Windows.Forms.TextBox();
+            this.labelApp = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelInterval = new System.Windows.Forms.Label();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownInterval)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.numericUpDownInterval);
+            this.groupBox3.Controls.Add(this.labelMinute);
+            this.groupBox3.Controls.Add(this.labelInterval);
+            this.groupBox3.Location = new System.Drawing.Point(28, 299);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Size = new System.Drawing.Size(398, 70);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            // 
+            // numericUpDownInterval
+            // 
+            this.numericUpDownInterval.Location = new System.Drawing.Point(106, 26);
+            this.numericUpDownInterval.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDownInterval.Maximum = new decimal(new int[] {
+            720,
+            0,
+            0,
+            0});
+            this.numericUpDownInterval.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownInterval.Name = "numericUpDownInterval";
+            this.numericUpDownInterval.Size = new System.Drawing.Size(72, 25);
+            this.numericUpDownInterval.TabIndex = 1;
+            this.numericUpDownInterval.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDownInterval.ValueChanged += new System.EventHandler(this.numericUpDownInterval_ValueChanged);
+            // 
+            // labelMinute
+            // 
+            this.labelMinute.AutoSize = true;
+            this.labelMinute.Location = new System.Drawing.Point(204, 30);
+            this.labelMinute.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelMinute.Name = "labelMinute";
+            this.labelMinute.Size = new System.Drawing.Size(22, 15);
+            this.labelMinute.TabIndex = 2;
+            this.labelMinute.Text = "秒";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.radioButtonNone);
+            this.groupBox2.Controls.Add(this.radioButtonHibernate);
+            this.groupBox2.Controls.Add(this.radioButtonShutdown);
+            this.groupBox2.Location = new System.Drawing.Point(28, 160);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(398, 114);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "判断条件";
+            // 
+            // radioButtonNone
+            // 
+            this.radioButtonNone.AutoSize = true;
+            this.radioButtonNone.Location = new System.Drawing.Point(241, 26);
+            this.radioButtonNone.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButtonNone.Name = "radioButtonNone";
+            this.radioButtonNone.Size = new System.Drawing.Size(88, 19);
+            this.radioButtonNone.TabIndex = 2;
+            this.radioButtonNone.TabStop = true;
+            this.radioButtonNone.Text = "进程名称";
+            this.radioButtonNone.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonHibernate
+            // 
+            this.radioButtonHibernate.AutoSize = true;
+            this.radioButtonHibernate.Location = new System.Drawing.Point(8, 26);
+            this.radioButtonHibernate.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButtonHibernate.Name = "radioButtonHibernate";
+            this.radioButtonHibernate.Size = new System.Drawing.Size(73, 19);
+            this.radioButtonHibernate.TabIndex = 0;
+            this.radioButtonHibernate.TabStop = true;
+            this.radioButtonHibernate.Text = "文件名";
+            this.radioButtonHibernate.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonShutdown
+            // 
+            this.radioButtonShutdown.AutoSize = true;
+            this.radioButtonShutdown.Location = new System.Drawing.Point(121, 26);
+            this.radioButtonShutdown.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButtonShutdown.Name = "radioButtonShutdown";
+            this.radioButtonShutdown.Size = new System.Drawing.Size(88, 19);
+            this.radioButtonShutdown.TabIndex = 1;
+            this.radioButtonShutdown.TabStop = true;
+            this.radioButtonShutdown.Text = "窗口标题";
+            this.radioButtonShutdown.UseVisualStyleBackColor = true;
+            // 
+            // textBoxApp
+            // 
+            this.textBoxApp.Location = new System.Drawing.Point(85, 26);
+            this.textBoxApp.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxApp.Name = "textBoxApp";
+            this.textBoxApp.Size = new System.Drawing.Size(293, 25);
+            this.textBoxApp.TabIndex = 1;
+            // 
+            // labelApp
+            // 
+            this.labelApp.AutoSize = true;
+            this.labelApp.Location = new System.Drawing.Point(25, 29);
+            this.labelApp.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelApp.Name = "labelApp";
+            this.labelApp.Size = new System.Drawing.Size(52, 15);
+            this.labelApp.TabIndex = 0;
+            this.labelApp.Text = "说明：";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Location = new System.Drawing.Point(28, 79);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(398, 59);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "调节方向";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(232, 26);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(118, 19);
+            this.radioButton2.TabIndex = 0;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "持续递减调节";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(50, 26);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(133, 19);
+            this.radioButton3.TabIndex = 1;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "触发锁屏时延时";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(106, 69);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(258, 25);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 72);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "条件值：";
+            // 
+            // labelInterval
+            // 
+            this.labelInterval.AutoSize = true;
+            this.labelInterval.Location = new System.Drawing.Point(8, 30);
+            this.labelInterval.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelInterval.Name = "labelInterval";
+            this.labelInterval.Size = new System.Drawing.Size(82, 15);
+            this.labelInterval.TabIndex = 0;
+            this.labelInterval.Text = "调节时间：";
+            this.labelInterval.Click += new System.EventHandler(this.labelInterval_Click);
+            // 
+            // AdjustmentConditionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 457);
+            this.ControlBox = false;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBoxApp);
+            this.Controls.Add(this.labelApp);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "AdjustmentConditionForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "调节项";
+            this.Load += new System.EventHandler(this.TaskSettingsForm_Load);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownInterval)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBoxApp;
+        private System.Windows.Forms.Label labelApp;
+        private System.Windows.Forms.NumericUpDown numericUpDownInterval;
+        private System.Windows.Forms.Label labelMinute;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButtonNone;
+        private System.Windows.Forms.RadioButton radioButtonHibernate;
+        private System.Windows.Forms.RadioButton radioButtonShutdown;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelInterval;
+    }
+}
