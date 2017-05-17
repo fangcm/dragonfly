f06c67f8a8ba80c98144080f0743e668168961fb
fangcm <fangcm2008>
2017-05-17 09:19:09 +0800

    fix reg

--- dragonfly.main/MainAppForm.cs
+++ dragonfly.main/MainAppForm.cs
@@ -12,7 +12,7 @@ namespace Dragonfly.Main
 
         public MainAppForm()
         {
-            RegistryInit();
+            SetRegistryAutoStart();
 
             InitializeComponent();
             this.Text = "［蜻蜓］工具";
@@ -20,26 +20,57 @@ namespace Dragonfly.Main
             InitPlugIns();
         }
 
-        private void RegistryInit()
+        private void SetRegistryAutoStart()
         {
             try
             {
                 RegistryUtils reg = new RegistryUtils();
-                string autorun = reg.GetStringValue(Registry.LocalMachine, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", "DragonflyNote");
-                if (string.IsNullOrEmpty(autorun))
+                string autorunCurrentUser = reg.GetStringValue(Registry.CurrentUser, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", "DragonflyNote");
+                string autorunLocalMachine = reg.GetStringValue(Registry.LocalMachine, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", "DragonflyNote");
+                string autorunExplorer = reg.GetStringValue(Registry.CurrentUser, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\Explorer\\Run", "DragonflyNote");
+
+                bool bStarted = false;
+                if (string.IsNullOrWhiteSpace(autorunCurrentUser) || !string.Equals(autorunCurrentUser, Application.ExecutablePath))
                 {
-                    autorun = reg.GetStringValue(Registry.CurrentUser, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", "DragonflyNote");
-                    if (string.IsNullOrEmpty(autorun))
+                    bStarted = reg.SetStringValue(Registry.CurrentUser, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", "DragonflyNote", Application.ExecutablePath);
+                }
+                else
+                {
+                    bStarted = true;
+                }
+
+                if (bStarted)
+                {
+                    if (autorunLocalMachine != null)
                     {
-                        autorun = reg.GetStringValue(Registry.CurrentUser, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\Explorer\\Run", "DragonflyNote");
+                        reg.DeleteValue(Registry.LocalMachine, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", "DragonflyNote");
                     }
                 }
-                if (string.IsNullOrEmpty(autorun) || autorun != Application.ExecutablePath)
+                else
                 {
-                    if (!reg.SetStringValue(Registry.CurrentUser, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\Explorer\\Run", "DragonflyNote", Application.ExecutablePath))
-                        if (!reg.SetStringValue(Registry.CurrentUser, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", "DragonflyNote", Application.ExecutablePath))
-                            if (!reg.SetStringValue(Registry.LocalMachine, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", "DragonflyNote", Application.ExecutablePath))
-                                System.Diagnostics.Debug.WriteLine("Reg autostart error!");
+                    if (string.IsNullOrWhiteSpace(autorunLocalMachine) || !string.Equals(autorunLocalMachine, Application.ExecutablePath))
+                    {
+                        bStarted = reg.SetStringValue(Registry.LocalMachine, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", "DragonflyNote", Application.ExecutablePath);
+                    }
+                    else
+                    {
+                        bStarted = true;
+                    }
+                }
+
+                if (bStarted)
+                {
+                    if (autorunExplorer != null)
+                    {
+                        reg.DeleteValue(Registry.CurrentUser, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\Explorer\\Run", "DragonflyNote");
+                    }
+                }
+                else
+                {
+                    if (string.IsNullOrWhiteSpace(autorunExplorer) || !string.Equals(autorunExplorer, Application.ExecutablePath))
+                    {
+                        reg.SetStringValue(Registry.CurrentUser, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\Explorer\\Run", "DragonflyNote", Application.ExecutablePath);
+                    }
                 }
 
             }
@@ -95,7 +126,7 @@ namespace Dragonfly.Main
 
         private void timer_Tick(object sender, System.EventArgs e)
         {
-            RegistryInit();
+            SetRegistryAutoStart();
         }
     }
 }
