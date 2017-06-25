ef20048b1ac52bb5a2346bdf9c5f81d31413986f
fangcm <fangcm2008@qq.com>
2017-06-25 13:55:39 +0800

    service

---
+++ dragonfly.service/dragonfly.service/DragonflyService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dragonfly.service
+{
+    public partial class DragonflyService : ServiceBase
+    {
+        public DragonflyService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
