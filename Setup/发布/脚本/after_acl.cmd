04b9c49e1ff0946a36105f5066fc6099794ef5c3
fangcm <fangcm2008>
2017-07-18 18:28:58 +0800

    v 1.1.0

--- Setup/发布/脚本/after_acl.cmd
+++ Setup/发布/脚本/after_acl.cmd
@@ -1,11 +1,23 @@
-c:\windows\system32\icacls.exe C:\Users\HEZY\AppData\Roaming\dragonfly\plugins\  /inheritance:d 
+set id=%USERNAME%
+c:\windows\system32\icacls.exe C:\Users\%id%\AppData\Roaming\dragonfly\plugins\  /inheritance:d
 
-c:\windows\system32\icacls.exe C:\Users\HEZY\AppData\Roaming\dragonfly\plugins\  /remove:g system
-c:\windows\system32\icacls.exe C:\Users\HEZY\AppData\Roaming\dragonfly\plugins\  /grant:r system:RX  /T /L /C /Q
+c:\windows\system32\icacls.exe C:\Users\%id%\AppData\Roaming\dragonfly\plugins\  /remove:g system
+c:\windows\system32\icacls.exe C:\Users\%id%\AppData\Roaming\dragonfly\plugins\  /grant:r system:RX  /T /L /C /Q
 
-c:\windows\system32\icacls.exe C:\Users\HEZY\AppData\Roaming\dragonfly\plugins\  /remove:g Administrators
-c:\windows\system32\icacls.exe C:\Users\HEZY\AppData\Roaming\dragonfly\plugins\  /grant:r Administrators:RX  /T /L /C /Q
+c:\windows\system32\icacls.exe C:\Users\%id%\AppData\Roaming\dragonfly\plugins\  /remove:g Administrators
+c:\windows\system32\icacls.exe C:\Users\%id%\AppData\Roaming\dragonfly\plugins\  /grant:r Administrators:RX  /T /L /C /Q
+c:\windows\system32\icacls.exe C:\Users\%id%\AppData\Roaming\dragonfly\plugins\  /remove:g %id%
+c:\windows\system32\icacls.exe C:\Users\%id%\AppData\Roaming\dragonfly\plugins\  /grant:r %id%:RX  /T /L /C /Q
 
-c:\windows\system32\icacls.exe C:\Users\HEZY\AppData\Roaming\dragonfly\plugins\  /remove:g hezy
-c:\windows\system32\icacls.exe C:\Users\HEZY\AppData\Roaming\dragonfly\plugins\  /grant:r hezy:RX  /T /L /C /Q
+c:\windows\system32\icacls.exe "C:\Program Files\dragonfly\"  /inheritance:d
+
+c:\windows\system32\icacls.exe "C:\Program Files\dragonfly\"  /remove:g system
+c:\windows\system32\icacls.exe "C:\Program Files\dragonfly\"  /grant:r system:RX  /T /L /C /Q
+
+c:\windows\system32\icacls.exe "C:\Program Files\dragonfly\"  /remove:g Administrators
+c:\windows\system32\icacls.exe "C:\Program Files\dragonfly\"  /grant:r Administrators:RX  /T /L /C /Q
+
+c:\windows\system32\icacls.exe "C:\Program Files\dragonfly\"  /remove:g %id%
+c:\windows\system32\icacls.exe "C:\Program Files\dragonfly\"  /grant:r %id%:RX  /T /L /C /Q
+
 
--- Setup/发布/脚本/before_acl.cmd
+++ Setup/发布/脚本/before_acl.cmd
@@ -1,15 +1,15 @@
+set id=%USERNAME% 
 taskkill /f /im simple.notify.exe
 taskkill /f /im questions.notify.exe
 taskkill /f /im dragonfly.plugin.task.notify.exe
 
+c:\windows\system32\icacls.exe C:\Users\%id%\AppData\Roaming\dragonfly\plugins\  /remove system
+c:\windows\system32\icacls.exe C:\Users\%id%\AppData\Roaming\dragonfly\plugins\  /grant:r system:F  /T /L /C /Q
 
-c:\windows\system32\icacls.exe C:\Users\HEZY\AppData\Roaming\dragonfly\plugins\  /remove system
-c:\windows\system32\icacls.exe C:\Users\HEZY\AppData\Roaming\dragonfly\plugins\  /grant:r system:F  /T /L /C /Q
+c:\windows\system32\icacls.exe C:\Users\%id%\AppData\Roaming\dragonfly\plugins\  /remove Administrators
+c:\windows\system32\icacls.exe C:\Users\%id%\AppData\Roaming\dragonfly\plugins\  /grant:r Administrators:F  /T /L /C /Q
 
-c:\windows\system32\icacls.exe C:\Users\HEZY\AppData\Roaming\dragonfly\plugins\  /remove Administrators
-c:\windows\system32\icacls.exe C:\Users\HEZY\AppData\Roaming\dragonfly\plugins\  /grant:r Administrators:F  /T /L /C /Q
+c:\windows\system32\icacls.exe C:\Users\%id%\AppData\Roaming\dragonfly\plugins\  /remove %id% 
+c:\windows\system32\icacls.exe C:\Users\%id%\AppData\Roaming\dragonfly\plugins\  /grant:r %id%:F  /T /L /C /Q
 
-c:\windows\system32\icacls.exe C:\Users\HEZY\AppData\Roaming\dragonfly\plugins\  /remove hezy
-c:\windows\system32\icacls.exe C:\Users\HEZY\AppData\Roaming\dragonfly\plugins\  /grant:r hezy:F  /T /L /C /Q
-
-c:\windows\system32\icacls.exe C:\Users\HEZY\AppData\Roaming\dragonfly\plugins\  /inheritance:e
+c:\windows\system32\icacls.exe C:\Users\%id%\AppData\Roaming\dragonfly\plugins\  /inheritance:e
