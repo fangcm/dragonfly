5c0e9a039b63e06c9b2e699f59de09d89e5e8b58
fangcm <fangcm2008@qq.com>
2017-08-19 12:25:26 +0800

    r

--- Setup/发布/脚本/after_acl.cmd
+++ Setup/发布/脚本/after_acl.cmd
@@ -1,25 +1,6 @@
-takeown /f %APPDATA%\dragonfly\plugins\* /r /d y
-icacls.exe %APPDATA%\dragonfly\plugins\  /inheritance:d 
+icacls.exe "%APPDATA%\dragonfly\plugins\*" /setowner %USERNAME% /T /L /C /Q
+icacls.exe "%APPDATA%\dragonfly\plugins\*" /inheritance:r /grant:r system:(WDAC,RX) Administrators:(WDAC,RX) %USERNAME%:(WDAC,RX) /T /L /C /Q
 
-icacls.exe %APPDATA%\dragonfly\plugins\  /remove:g system
-icacls.exe %APPDATA%\dragonfly\plugins\  /grant:r system:RX  /T /L /C /Q
-
-icacls.exe %APPDATA%\dragonfly\plugins\  /remove:g Administrators
-icacls.exe %APPDATA%\dragonfly\plugins\  /grant:r Administrators:RX  /T /L /C /Q
-
-icacls.exe %APPDATA%\dragonfly\plugins\  /remove:g %USERNAME%
-icacls.exe %APPDATA%\dragonfly\plugins\  /grant:r %USERNAME%:RX  /T /L /C /Q
-
-takeown /f %ProgramFiles%\dragonfly\* /r /d y
-icacls.exe %ProgramFiles%\dragonfly\  /inheritance:d
-
-icacls.exe %ProgramFiles%\dragonfly\  /remove:g system
-icacls.exe %ProgramFiles%\dragonfly\  /grant:r system:RX  /T /L /C /Q
-
-icacls.exe %ProgramFiles%\dragonfly\  /remove:g Administrators
-icacls.exe %ProgramFiles%\dragonfly\  /grant:r Administrators:RX  /T /L /C /Q
-
-icacls.exe %ProgramFiles%\dragonfly\  /remove:g %id%
-icacls.exe %ProgramFiles%\dragonfly\  /grant:r %id%:RX  /T /L /C /Q
-
+icacls.exe "%ProgramFiles%\dragonfly\*" /setowner %USERNAME% /T /L /C /Q
+icacls.exe "%ProgramFiles%\dragonfly\*" /inheritance:r /grant:r system:(WDAC,RX) Administrators:(WDAC,RX) %USERNAME%:(WDAC,RX) /T /L /C /Q
 
--- Setup/发布/脚本/before_acl.cmd
+++ Setup/发布/脚本/before_acl.cmd
@@ -2,13 +2,4 @@ taskkill /f /im simple.notify.exe
 taskkill /f /im questions.notify.exe
 taskkill /f /im dragonfly.plugin.task.notify.exe
 
-icacls.exe %APPDATA%\dragonfly\plugins\  /remove system
-icacls.exe %APPDATA%\dragonfly\plugins\  /grant:r system:F  /T /L /C /Q
-
-icacls.exe %APPDATA%\dragonfly\plugins\  /remove Administrators
-icacls.exe %APPDATA%\dragonfly\plugins\  /grant:r Administrators:F  /T /L /C /Q
-
-icacls.exe %APPDATA%\dragonfly\plugins\  /remove %USERNAME%
-icacls.exe %APPDATA%\dragonfly\plugins\  /grant:r %USERNAME%:F  /T /L /C /Q
-
-icacls.exe %APPDATA%\dragonfly\plugins\  /inheritance:e
+icacls.exe "%APPDATA%\dragonfly\plugins\*" /grant:r system:(WDAC,F) Administrators:(WDAC,F) %USERNAME%:(WDAC,F) /T /L /C /Q
