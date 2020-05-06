9f43707af8fbbeb2da946af4da73684b9be8f245
fangcm <fangcm2008@qq.com>
2020-05-06 22:48:56 +0800

    o

--- plugins/grid_trading/plugin.grid_trading/TradingWorker.cs
+++ plugins/grid_trading/plugin.grid_trading/TradingWorker.cs
@@ -21,40 +21,109 @@ namespace Dragonfly.Plugin.GridTrading
                 return;
             }
 
+            List<Grid> gridsForA = grids.FindAll(x => x.StockMarket == StockMarket.A);
+            ProgressMarket(StockMarket.A, gridsForA);
+            List<Grid> gridsForHgt = grids.FindAll(x => x.StockMarket == StockMarket.Hgt);
+            ProgressMarket(StockMarket.Hgt, gridsForHgt);
+            List<Grid> gridsForSgt = grids.FindAll(x => x.StockMarket == StockMarket.Sgt);
+            ProgressMarket(StockMarket.Sgt, gridsForSgt);
+        }
+
+
+        private static void ProgressMarket(StockMarket market, List<Grid> grids)
+        {
+            if (grids == null || grids.Count == 0)
+            {
+                LoggerUtil.Log(LoggType.Gray, "无网格策略,在市场：" + market.ToString());
+                return;
+            }
+
             // 查询当日最后成交（同步当日成交记录）
             List<ModelTodayDeals> todayDeals;
 
-            todayDeals = TraderHelper.Instance.TodayDealsList();
-            TradeDao.SaveOrUpdateTodayDeals(StockMarket.A,todayDeals);
-
-            todayDeals = TraderHelper.Instance.HgtTodayDealsList();
-            TradeDao.SaveOrUpdateTodayDeals(StockMarket.Hgt, todayDeals);
-
-            todayDeals = TraderHelper.Instance.SgtTodayDealsList();
-            TradeDao.SaveOrUpdateTodayDeals(StockMarket.Sgt, todayDeals);
-
             // 查询当日可撤委托
-            List<ModelRevocableOrder> revocableOrderA = TraderHelper.Instance.RevocableOrders();
-            List<ModelRevocableOrder> revocableOrderHgt = TraderHelper.Instance.HgtRevocableOrders();
-            List<ModelRevocableOrder> revocableOrderSgt = TraderHelper.Instance.SgtRevocableOrders();
+            List<ModelRevocableOrder> revocableOrders;
 
-            foreach(Grid grid in grids) {
+            // 查询持仓数据
+            Tuple<ModelAccountStat, List<ModelHoldingStock>> holdingStocks;
+
+            switch (market)
+            {
+                case StockMarket.A:
+                    todayDeals = TraderHelper.Instance.TodayDealsList();
+                    TradeDao.SaveOrUpdateTodayDeals(StockMarket.A, todayDeals);
+                    revocableOrders = TraderHelper.Instance.RevocableOrders();
+                    holdingStocks = TraderHelper.Instance.HoldingStockList();
+                    break;
+                case StockMarket.Hgt:
+                    todayDeals = TraderHelper.Instance.HgtTodayDealsList();
+                    TradeDao.SaveOrUpdateTodayDeals(StockMarket.Hgt, todayDeals);
+                    revocableOrders = TraderHelper.Instance.HgtRevocableOrders();
+                    holdingStocks = TraderHelper.Instance.HgtHoldingStockList();
+                    break;
+                case StockMarket.Sgt:
+                    todayDeals = TraderHelper.Instance.SgtTodayDealsList();
+                    TradeDao.SaveOrUpdateTodayDeals(StockMarket.Sgt, todayDeals);
+                    revocableOrders = TraderHelper.Instance.SgtRevocableOrders();
+                    holdingStocks = TraderHelper.Instance.SgtHoldingStockList();
+                    break;
+            }
+
+
+
+
+            foreach (Grid grid in grids)
+            {
                 // 查出历史最后成交
                 TradingRecord tr = TradeDao.FindLastTradingRecord(grid.StockMarket, grid.StockCode);
                 grid.LastTradingOrder = new GridOrder() { Direction =, Price =, Volume = };
 
                 // 算出下一笔的买卖单
-                grid.ExpectedGridNode(price)
-                grid.ExpectedGridNode(holdingVolume)
+                GridNode nodeByPrice = grid.ExpectedGridNode(price);
+                GridNode nodeByVolume = grid.ExpectedGridNode(holdingVolume);
+                // 不一致, 忽略
 
 
-                // （同步委托记录），是否已经下单
+                // 对比委托记录，是否已经下单
 
 
 
-                // 下单，保存委托记录
+                // 下单，（保存委托记录，仅作为日志）
+                switch (grid.StockMarket)
+                {
+                    case StockMarket.A:
+                        if (nodeByPrice.BuyOrder != null)
+                        {
+                            TraderHelper.Instance.BuyStock(grid.StockCode, nodeByPrice.BuyOrder.Price, nodeByPrice.BuyOrder.Volume);
+                        }
+                        if (nodeByPrice.BuyOrder != null)
+                        {
+                            TraderHelper.Instance.SellStock(grid.StockCode, nodeByPrice.SellOrder.Price, nodeByPrice.SellOrder.Volume);
+                        }
+                        break;
+                    case StockMarket.Hgt:
+                        if (nodeByPrice.BuyOrder != null)
+                        {
+                            TraderHelper.Instance.HgtBuyStock(grid.StockCode, nodeByPrice.BuyOrder.Price, nodeByPrice.BuyOrder.Volume);
+                        }
+                        if (nodeByPrice.BuyOrder != null)
+                        {
+                            TraderHelper.Instance.HgtSellStock(grid.StockCode, nodeByPrice.SellOrder.Price, nodeByPrice.SellOrder.Volume);
+                        }
+                        break;
+                    case StockMarket.Sgt:
+                        if (nodeByPrice.BuyOrder != null)
+                        {
+                            TraderHelper.Instance.SgtBuyStock(grid.StockCode, nodeByPrice.BuyOrder.Price, nodeByPrice.BuyOrder.Volume);
+                        }
+                        if (nodeByPrice.BuyOrder != null)
+                        {
+                            TraderHelper.Instance.SgtSellStock(grid.StockCode, nodeByPrice.SellOrder.Price, nodeByPrice.SellOrder.Volume);
+                        }
+                        break;
+                }
             }
-
         }
+
     }
 }
