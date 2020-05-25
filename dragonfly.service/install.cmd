2e23f860fe9938cbf1a6d12cc0a4c7668732db86
fangcm <fangcm2008@qq.com>
2020-05-25 17:20:02 +0800

    0

--- dragonfly.service/install.cmd
+++
@@ -1,5 +0,0 @@
-
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /install %~dp0\dragonfly.service\bin\Debug\dsmanager.exe
-
-net start "dsmanager"
-pause
--- dragonfly.service/uninstall.cmd
+++
@@ -1,3 +0,0 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /uninstall %~dp0\dragonfly.service\bin\Debug\dsmanager.exe
-
-pause
